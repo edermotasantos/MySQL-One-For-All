f399b1f83b85f1576e355742951d32c217e4ad58
Eder Santos <3derms@gmail.com>
2021-09-28 18:38:47 -0300

    Cria uma VIEW chamada estatisticas_musicais que exiba três colunas

---
+++ desafio2.sql
@@ -0,0 +1,12 @@
+CREATE VIEW estatisticas_musicais AS 
+SELECT COUNT(*) AS cancoes,
+(
+SELECT COUNT(*)
+FROM SpotifyClone.artistas
+) AS artistas,
+(
+SELECT COUNT(*)
+FROM SpotifyClone.albuns
+) AS albuns
+FROM SpotifyClone.cancoes;
+ 
