cab9fd63a59921737ced8c37f64edc4b5c7fde39
Eder Santos <3derms@gmail.com>
2021-09-29 12:50:40 -0300

    Update desafio8.sql

--- desafio8.sql
+++ desafio8.sql
@@ -3,13 +3,13 @@ USE SpotifyClone;
 DELIMITER $$
 
 CREATE TRIGGER trigger_usuario_delete
-	BEFORE DELETE ON usuarios
-    FOR EACH ROW
+BEFORE DELETE ON usuarios
+FOR EACH ROW
 BEGIN
-	DELETE FROM cancoes_usuarios
-    WHERE usuario_id = OLD.usuario_id;
-    DELETE FROM usuarios_artistas
-    WHERE usuario_id = OLD.usuario_id;
+DELETE FROM cancoes_usuarios
+WHERE usuario_id = OLD.usuario_id;
+DELETE FROM usuarios_artistas
+WHERE usuario_id = OLD.usuario_id;
 END $$
 
 DELIMITER ;
