aaaf4cbb95508b22aa1dc5f69da54ac775f2318d
Eder Santos <3derms@gmail.com>
2021-09-29 13:40:26 -0300

    Corrige filtragem em albuns_do_artista

--- desafio9.sql
+++ desafio9.sql
@@ -7,7 +7,7 @@ BEGIN SELECT ar.artista AS artista, al.album AS album
 FROM artistas AS ar
 INNER JOIN albuns AS al
 ON al.artista_id = ar.album_id
-WHERE ar.artista LIKE nome_artista
+WHERE ar.artista LIKE CONCAT('%', nome_artista, '%')
 ORDER BY album;
 END $$
 
