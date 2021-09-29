c2f014471cda6c52f8570b90a9e4f3860cb17c40
Eder Santos <3derms@gmail.com>
2021-09-29 13:32:04 -0300

    Cria uma procedure chamada albuns_do_artista

---
+++ desafio9.sql
@@ -0,0 +1,16 @@
+USE SpotifyClone;
+
+DELIMITER $$
+
+CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(50))
+BEGIN SELECT ar.artista AS artista, al.album AS album
+FROM artistas AS ar
+INNER JOIN albuns AS al
+ON al.artista_id = ar.album_id
+WHERE ar.artista LIKE nome_artista
+ORDER BY album;
+END $$
+
+DELIMITER ;
+
+CALL  albuns_do_artista('Walter Phoenix');
