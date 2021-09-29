4ee332d3b7854d7103a8278037b74892f7664670
Eder Santos <3derms@gmail.com>
2021-09-29 14:12:25 -0300

    Update desafio9.sql

--- desafio9.sql
+++ desafio9.sql
@@ -6,7 +6,7 @@ CREATE PROCEDURE albuns_do_artista(IN nome_artista VARCHAR(200))
 BEGIN SELECT ar.artista AS artista, al.album AS album
 FROM artistas AS ar
 INNER JOIN albuns AS al
-ON al.artista_id = ar.album_id
+ON al.artista_id = ar.artista_id
 WHERE ar.artista LIKE CONCAT('%', nome_artista, '%')
 ORDER BY album;
 END $$
